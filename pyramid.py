de018584324c82b0d48b2fffe2bcd471b5d78d75
hoangjohn113 <hoangjohn113@gmail.com>
2022-05-20 15:20:51 -0400

    Update pyramid.py

--- pyramid.py
+++ pyramid.py
@@ -7,3 +7,4 @@ maxSymbol = int(input("How big do you wants the base to be? : "))
 for i in range(int(maxSymbol/2)):
     print (indent*int((maxSymbol/2)-i) + symbol*(i*2) + indent*int((maxSymbol/2)-i))
     time.sleep(0.5)
+#.
