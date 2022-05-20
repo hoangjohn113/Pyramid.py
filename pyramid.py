ce19910cba6b2eb69f226a452829146fed743fee
hoangjohn113 <hoangjohn113@gmail.com>
2022-05-20 15:02:25 -0400

    Create pyramid.py

---
+++ pyramid.py
@@ -0,0 +1,9 @@
+import time
+
+symbol = input("What symbol to use ? ")
+indent = " "
+maxSymbol = int(input("How big do you wants the base to be? : "))
+    
+for i in range(int(maxSymbol/2)):
+    print (indent*int((maxSymbol/2)-i) + symbol*(i*2) + indent*int((maxSymbol/2)-i))
+    time.sleep(0.5)
